8ad481d0d936e7176309b3ded65099285e25af46
Minjoong Kim <minjoong.slow@gmail.com>
2017-01-26 01:17:32 +0900

    Repository 인터페이스 추가

---
+++ src/main/java/readinglist/ReadingListRepository.java
@@ -0,0 +1,12 @@
+package readinglist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Created by minjoong on 2017. 1. 26..
+ */
+public interface ReadingListRepository extends JpaRepository<Book, Long> {
+    List<Book> findByReader(String reader);
+}
