4ab813572b6517a4c3f31200755bf3e7fb828bee
Minjoong Kim <minjoong.slow@gmail.com>
2017-05-22 03:01:24 +0900

    IntelliJ의 Inspect Code 기능을 이용하여 코드 리팩토링

--- src/main/java/readinglist/SecurityConfig.java
+++ src/main/java/readinglist/SecurityConfig.java
@@ -4,12 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 /**
  * Created by minjoong on 2017. 1. 29..
@@ -18,8 +14,13 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final ReaderRepository readerRepository;
+
     @Autowired
-    private ReaderRepository readerRepository;
+    public SecurityConfig(ReaderRepository readerRepository) {
+        this.readerRepository = readerRepository;
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -37,13 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(new UserDetailsService() {
-            @Override
-            public UserDetails loadUserByUsername(String username)
-                    throws UsernameNotFoundException {
-                return readerRepository.findOne(username);
-            }
-        });
+        auth.userDetailsService(readerRepository::findOne);
     }
 
 }
